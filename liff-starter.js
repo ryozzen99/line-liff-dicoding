3d27567e074aa603e29558124edafb263439e8e7
stryow22 <satriyowitjaksono2205@gmail.com>
2020-12-18 20:12:18 +0700

    make it better

--- liff-starter.js
+++ liff-starter.js
@@ -156,7 +156,7 @@ function registerButtonHandlers() {
                     // list_data[i].nama + ' ' + list_data[i].agenda
                     // $('#list-catatan').html(data_app)
                     // list_data.length > 0
-                    localStorage.getItem("list_data[i].nama + ' ' + list_data[i].agenda")
+                    localStorage.getItem('list_data')
             }]).then(function () {
                 window.alert('List Belanjaan anda sudah dikirimkan.');
             }).catch(function (error) {
